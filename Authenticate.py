5d8ec4bddfb45e8b5d087761670d0c40feaf61b7
Vinicius Ferri <142631368+FerriVinicius@users.noreply.github.com>
2023-11-23 19:30:16 -0300

    Update Authenticate.py

--- Authenticate.py
+++ Authenticate.py
@@ -1,10 +1,12 @@
+##importa bibliotecas úteis para a funcionalidade
 import hmac
 import streamlit as st
 
-
+##função de autenticação por meio de um nome de usuário e uma senha
 def check_password():
-    """Returns `True` if the user had a correct password."""
-
+    ##retorna True se o usuário fornecer uma senha correta
+    
+    ##esconde a barra lateral de navegação enquanto o usuário não estiver autenticado
     hide_bar= """
         <style>
         [data-testid="stSidebar"][aria-expanded="true"] > div:first-child {
@@ -16,55 +18,58 @@ def check_password():
         }
         </style>
     """
-
-    # Initialize session state variables if they don't exist
+    
+    ##aspectos visuais da sidebar
+    st.sidebar.header("Bem vindo!")
+    icon = "https://minhabiblioteca.com.br/wp-content/uploads/2021/04/logo-einstein.png"
+    st.sidebar.image(icon, use_column_width=True)
+    
+    ##inicializa a sessão caso ela não exista
     if "show_error_message" not in st.session_state:
         st.session_state.show_error_message = False
     if "password_correct" not in st.session_state:
         st.session_state.password_correct = None
 
+    ##função para formulário de login
     def login_form():
-        """Form with widgets to collect user information"""
         with st.form("Credentials"):
             st.text_input("Username", key="username")
             st.text_input("Password", type="password", key="password")
             st.form_submit_button("Log in", on_click=password_entered)
             st.markdown(hide_bar, unsafe_allow_html=True)
-
+    
+    #função para checar as credenciais inseridas no sistema, retorna True caso esteja correta
     def password_entered():
-        """Checks whether a password entered by the user is correct."""
         if st.session_state["username"] in st.secrets["passwords"] and hmac.compare_digest(
                 st.session_state["password"],
                 st.secrets.passwords[st.session_state["username"]],
         ):
             st.session_state.password_correct = True
-            del st.session_state["password"]  # Don't store the username or password.
+            del st.session_state["password"]  ##funcionalidade para não salvar as credenciais utilizadas
             del st.session_state["username"]
         else:
             st.session_state.password_correct = False
             st.session_state.show_error_message = True
             st.markdown(hide_bar, unsafe_allow_html=True)
 
+    ##função para sair do sistema e retornar para a página de login
     def logout():
-        """Logs the user out and returns to the login form."""
         st.session_state.password_correct = None
         st.session_state.show_error_message = False
-    
-    st.sidebar.header(f"Bem vindo!")
-    icon = "/Users/Admin/Documents/pages/icon.png"
-    st.sidebar.image(icon, use_column_width=True)
-    
-    # Return True if the username + password is validated.
+       
+    ##complemento da função check_password(), com inserção de botão para encerramento de sessão
     if st.session_state.password_correct is True:
-        st.sidebar.button("Sair", on_click=logout)
+        st.sidebar.button("❌ Encerrar Sessão", on_click=logout)
         return True
 
-    # Show inputs for username + password.
+    ##completmento da função check_password(), retorna mensagem de erro ao inserir credenciais inválidas.
     login_form()
     if st.session_state.show_error_message:
         st.error("Usuário ou senha inválidas, tente novamente.")
         st.markdown(hide_bar, unsafe_allow_html=True)
-    return False
-
-if not check_password():
-    st.stop()
+    
+    # Verifica se as credenciais são corretas antes de continuar
+    if st.session_state.password_correct is True:
+        return True
+    else:
+        return False
