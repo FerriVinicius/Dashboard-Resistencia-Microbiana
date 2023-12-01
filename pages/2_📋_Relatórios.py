9be8a99655b845996adbbe83420d48614c50b7ad
Vinicius Ferri <142631368+FerriVinicius@users.noreply.github.com>
2023-12-01 10:26:33 -0300

    Update 2_📋_Relatórios.py

--- pages/2_📋_Relatórios.py
+++ pages/2_📋_Relatórios.py
@@ -1,21 +1,15 @@
 ##importa bibliotecas e funções úteis para o programa
 import streamlit as st
 import pandas as pd
-import base64
 from io import BytesIO
 from Authenticate import check_password
-from pandas.api.types import (
-    is_categorical_dtype,
-    is_datetime64_any_dtype,
-    is_numeric_dtype,
-    is_object_dtype,
-)
 
 ##muda o título da página na aba do navegador
 st.set_page_config(
     page_title="Relatórios - Einstein PMRM",
     page_icon="📋",
     )
+st.header("Programa de Monitoramento de Resistência Microbiana")
 
 ##esconde a barra de acesso lateral durante o login do usuário
 hide_bar= """
@@ -29,7 +23,7 @@ hide_bar= """
     }
     </style>
 """
-if check_password() == True:
+if check_password():
     def filter_dataframe(df: pd.DataFrame) -> pd.DataFrame:
         modify = st.checkbox("Adicionar Filtros")
         
@@ -38,24 +32,19 @@ if check_password() == True:
         
         df = df.copy()
         
-        for col in df.columns:
-            if pd.api.types.is_object_dtype(df[col]):
-                try:
-                    df[col] = pd.to_datetime(df[col])
-                except Exception:
-                    pass
-    
-            if pd.api.types.is_datetime64_any_dtype(df[col]):
-                df[col] = df[col].dt.tz_localize(None)
-        
         modification_container = st.container()
-        
+    
         with modification_container:
-            to_filter_columns = st.multiselect("Filtrar tabela por:", df.columns)
-            for column in to_filter_columns:
+            to_filter_columns = st.multiselect("Filtrar tabela por:", list(column_mapping.values()))
+            to_filter_columns = [key for key, value in column_mapping.items() if value in to_filter_columns]
+            
+            for column_key in to_filter_columns:
+                # Use a chave mapeada para acessar a coluna
+                column = column_mapping[column_key]
+                
                 left, right = st.columns((1, 20))
                 left.write("↳")
-                
+    
                 if pd.api.types.is_categorical_dtype(df[column]) or df[column].nunique() < 10:
                     user_cat_input = right.multiselect(
                         f"Valores para {column}",
@@ -89,26 +78,58 @@ if check_password() == True:
                         df = df.loc[df[column].between(start_date, end_date)]
                 else:
                     user_text_input = right.text_input(
-                        f"Substring or regex in {column}",
+                        f"Filtros adicionais {column}",
                     )
                     if user_text_input:
                         df = df[df[column].str.contains(user_text_input)]
-        
-        if st.button("Exportar para Excel"):
-            # Exporta o DataFrame filtrado para um arquivo Excel
-            excel_data = BytesIO()
-            df.to_excel(excel_data, index=False, engine='openpyxl')
-            b64 = base64.b64encode(excel_data.getvalue()).decode()
-            href = f'<a href="data:application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;base64,{b64}" download="filtered_data.xlsx">Download Excel</a>'
-            st.markdown(href, unsafe_allow_html=True)
-        
-        return df
     
+            # Adiciona o botão de exportação
+            export_button = st.button("Preparar dados para exportação")
+    
+            # Se o botão for pressionado, exporta o DataFrame
+            if export_button:
+                export_data = BytesIO()
+                df.to_csv(export_data, index=False)
+                export_data.seek(0)
+                st.download_button(
+                    label="Download",
+                    data=export_data,
+                    file_name="dados_filtrados.csv",
+                    key="download_button",
+                )
+            
+            return df
+    
+    # Mapeamento de colunas para nomes mais usuais
+    column_mapping = {
+        'dh_admissao_paciente': 'Data de Admissão do Paciente',
+        'dh_alta_paciente': 'Data de Alta do Paciente',
+        'ds_tipo_encontro': 'Tipo de Encontro',
+        'ds_unidade_coleta': 'Unidade de Coleta',
+        'ds_predio_coleta': 'Prédio de Coleta',
+        'ds_ala_coleta': 'Ala de Coleta',
+        'ds_quarto_coleta': 'Quarto de Coleta',
+        'ds_leito_coleta': 'Leito de Coleta',
+        'dh_coleta_exame': 'Data e Hora de Coleta do Exame',
+        'cd_sigla_microorganismo': 'Sigla do Microorganismo',
+        'ds_micro_organismo': 'Microorganismo',
+        'ds_antibiotico_microorganismo': 'Antibiótico para o Microorganismo',
+        'cd_interpretacao_antibiograma': 'Interpretação do Antibiograma',
+        'ic_crescimento_microorganismo': 'Crescimento do Microorganismo',
+        'ds_resultado_exame': 'Resultado do Exame',
+    }
+
     # Carrega os dados do arquivo CSV no sistema
     df = pd.read_csv("https://raw.githubusercontent.com/AndersonEduardo/pbl-2023/main/sample_data_clean.csv")
-    
+
+    # Filtra apenas as colunas mencionadas no column_mapping
+    filtered_df = df[list(column_mapping.keys())]
+
+    # Renomeia as colunas conforme o mapeamento
+    filtered_df = filtered_df.rename(columns=column_mapping)
+
     # Mostra o DataFrame filtrado e adiciona a funcionalidade de exportação
-    filtered_df = filter_dataframe(df)
+    filtered_df = filter_dataframe(filtered_df)
     st.dataframe(filtered_df)
 else:
     st.stop()
