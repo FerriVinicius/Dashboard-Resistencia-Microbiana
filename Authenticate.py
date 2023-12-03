3f00e77d8ea0fc6874d78dcf44a965d0181b7e02
Vinicius Ferri <142631368+FerriVinicius@users.noreply.github.com>
2023-12-02 21:47:38 -0300

    Update Authenticate.py

--- Authenticate.py
+++ Authenticate.py
@@ -1,12 +1,12 @@
-##importa bibliotecas úteis para a funcionalidade
+##importa bibliotecas Ãºteis para a funcionalidade
 import hmac
 import streamlit as st
 
-##função de autenticação por meio de um nome de usuário e uma senha
+##funÃ§Ã£o de autenticaÃ§Ã£o por meio de um nome de usuÃ¡rio e uma senha
 def check_password():
-    ##retorna True se o usuário fornecer uma senha correta
+    ##retorna True se o usuÃ¡rio fornecer uma senha correta
     
-    ##esconde a barra lateral de navegação enquanto o usuário não estiver autenticado
+    ##esconde a barra lateral de navegaÃ§Ã£o enquanto o usuÃ¡rio nÃ£o estiver autenticado
     hide_bar= """
         <style>
         [data-testid="stSidebar"][aria-expanded="true"] > div:first-child {
@@ -19,56 +19,77 @@ def check_password():
         </style>
     """
     
+    page_bg_img = f"""
+    <style>
+    [data-testid="stAppViewContainer"] > .main {{
+    background-image: url("https://futurodasaude.com.br/wp-content/uploads/2023/08/Formacao-de-medicos-Einstein.jpeg");
+    background-size: 150%;
+    background-position: center;
+    background-repeat: no-repeat;
+    background-attachment: local;
+    }}
+    
+    [data-testid="stHeader"] {{
+    background: rgba(0,0,0,0);
+    }}
+    
+    [data-testid="stToolbar"] {{
+    right: 2rem;
+    }}
+    """
+    
+    st.markdown(page_bg_img, unsafe_allow_html=True)
+    
     ##aspectos visuais da sidebar
     st.sidebar.header("Bem vindo!")
     icon = "https://minhabiblioteca.com.br/wp-content/uploads/2021/04/logo-einstein.png"
     st.sidebar.image(icon, use_column_width=True)
     
-    ##inicializa a sessão caso ela não exista
+    ##inicializa a sessÃ£o caso ela nÃ£o exista
     if "show_error_message" not in st.session_state:
         st.session_state.show_error_message = False
     if "password_correct" not in st.session_state:
         st.session_state.password_correct = None
 
-    ##função para formulário de login
+    ##funÃ§Ã£o para formulÃ¡rio de login
     def login_form():
         with st.form("Credentials"):
-            st.text_input("Username", key="username")
-            st.text_input("Password", type="password", key="password")
-            st.form_submit_button("Log in", on_click=password_entered)
+            st.text_input("Usuário", key="username")
+            st.text_input("Senha", type="password", key="password")
+            st.form_submit_button("Entrar", on_click=password_entered)
             st.markdown(hide_bar, unsafe_allow_html=True)
     
-    #função para checar as credenciais inseridas no sistema, retorna True caso esteja correta
+    #funÃ§Ã£o para checar as credenciais inseridas no sistema, retorna True caso esteja correta
     def password_entered():
         if st.session_state["username"] in st.secrets["passwords"] and hmac.compare_digest(
                 st.session_state["password"],
                 st.secrets.passwords[st.session_state["username"]],
         ):
             st.session_state.password_correct = True
-            del st.session_state["password"]  ##funcionalidade para não salvar as credenciais utilizadas
+            del st.session_state["password"]  ##funcionalidade para nÃ£o salvar as credenciais utilizadas
             del st.session_state["username"]
         else:
             st.session_state.password_correct = False
             st.session_state.show_error_message = True
             st.markdown(hide_bar, unsafe_allow_html=True)
 
-    ##função para sair do sistema e retornar para a página de login
+    ##funÃ§Ã£o para sair do sistema e retornar para a pÃ¡gina de login
     def logout():
         st.session_state.password_correct = None
         st.session_state.show_error_message = False
        
-    ##complemento da função check_password(), com inserção de botão para encerramento de sessão
+    ##complemento da funÃ§Ã£o check_password(), com inserÃ§Ã£o de botÃ£o para encerramento de sessÃ£o
     if st.session_state.password_correct is True:
         st.sidebar.button("❌ Encerrar Sessão", on_click=logout)
         return True
 
-    ##completmento da função check_password(), retorna mensagem de erro ao inserir credenciais inválidas.
+    ##completmento da funÃ§Ã£o check_password(), retorna mensagem de erro ao inserir credenciais invÃ¡lidas.
     login_form()
     if st.session_state.show_error_message:
-        st.error("Usuário ou senha inválidas, tente novamente.")
+        st.error("UsuÃ¡rio ou senha invÃ¡lidas, tente novamente.")
         st.markdown(hide_bar, unsafe_allow_html=True)
     
-    # Verifica se as credenciais são corretas antes de continuar
+    # Verifica se as credenciais sÃ£o corretas antes de continuar
     if st.session_state.password_correct is True:
         return True
     else:
