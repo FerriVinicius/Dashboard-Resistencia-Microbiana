78fee4eec3d0248fa483da8ca05d7119d2f4f637
Vinicius Ferri <142631368+FerriVinicius@users.noreply.github.com>
2023-12-07 22:38:55 -0300

    Update 2_📋_Relatórios.py

--- pages/2_📋_Relatórios.py
+++ pages/2_📋_Relatórios.py
@@ -1,19 +1,19 @@
-##importa bibliotecas e funções úteis para o programa
+# Importa bibliotecas e funções úteis para o programa
 import streamlit as st
 import pandas as pd
 from io import BytesIO
 from Authenticate import check_password
 
-##muda o título da página na aba do navegador
+# Muda o título da página na aba do navegador
 st.set_page_config(
     page_title="Relatórios - Einstein PMRM",
     page_icon="📋",
     layout='wide',
-    )
+)
 st.header("Programa de Monitoramento de Resistência Microbiana", divider='green')
 
-##esconde a barra de acesso lateral durante o login do usuário
-hide_bar= """
+# Esconde a barra de acesso lateral durante o login do usuário
+hide_bar = """
     <style>
     [data-testid="stSidebar"][aria-expanded="true"] > div:first-child {
         visibility:hidden;
@@ -25,14 +25,14 @@ hide_bar= """
     </style>
 """
 
-if check_password():
+if check_password() == True:
     page_bg_img = f"""
     <style>
     [data-testid="stAppViewContainer"] > .main {{
-    background-image: url("https://www.willametteinnovators.com/wp-content/uploads/2019/07/Capture2-600x425@2x.png");
-    background-size: 150%;
+    background-image: url("https://raw.githubusercontent.com/FerriVinicius/Dashboard-Resistencia-Microbiana/main/151537457_l_normal_none.jpg");
+    background-size: contain;
     background-position: center;
-    background-repeat: no-repeat;
+    background-repeat: repeat;
     background-attachment: local;
     }}
     [data-testid="stSidebar"] > div:first-child {{
@@ -41,94 +41,29 @@ if check_password():
     background-repeat: no-repeat;
     background-attachment: fixed;
     }}
-    
+
     [data-testid="stHeader"] {{
     background: rgba(0,0,0,0);
     }}
-    
+
     [data-testid="stToolbar"] {{
     right: 2rem;
     }}
     """
-    
+
     st.markdown(page_bg_img, unsafe_allow_html=True)
-    def filter_dataframe(df: pd.DataFrame) -> pd.DataFrame:
-        modify = st.checkbox("Adicionar Filtros")
-        
-        if not modify:
-            return df
-        
-        df = df.copy()
-        
-        modification_container = st.container()
-    
-        with modification_container:
-            to_filter_columns = st.multiselect("Filtrar tabela por:", list(column_mapping.values()))
-            to_filter_columns = [key for key, value in column_mapping.items() if value in to_filter_columns]
-            
-            for column_key in to_filter_columns:
-                # Use a chave mapeada para acessar a coluna
-                column = column_mapping[column_key]
-                
-                left, right = st.columns((1, 20))
-                left.write("↳")
-    
-                if pd.api.types.is_categorical_dtype(df[column]) or df[column].nunique() < 10:
-                    user_cat_input = right.multiselect(
-                        f"Valores para {column}",
-                        df[column].unique(),
-                        default=list(df[column].unique()),
-                    )
-                    df = df[df[column].isin(user_cat_input)]
-                elif pd.api.types.is_numeric_dtype(df[column]):
-                    _min = float(df[column].min())
-                    _max = float(df[column].max())
-                    step = (_max - _min) / 100
-                    user_num_input = right.slider(
-                        f"Valores para {column}",
-                        _min,
-                        _max,
-                        (_min, _max),
-                        step=step,
-                    )
-                    df = df[df[column].between(*user_num_input)]
-                elif pd.api.types.is_datetime64_any_dtype(df[column]):
-                    user_date_input = right.date_input(
-                        f"Valores para {column}",
-                        value=(
-                            df[column].min(),
-                            df[column].max(),
-                        ),
-                    )
-                    if len(user_date_input) == 2:
-                        user_date_input = tuple(map(pd.to_datetime, user_date_input))
-                        start_date, end_date = user_date_input
-                        df = df.loc[df[column].between(start_date, end_date)]
-                else:
-                    user_text_input = right.text_input(
-                        f"Filtros adicionais {column}",
-                    )
-                    if user_text_input:
-                        df = df[df[column].str.contains(user_text_input)]
-    
-            # Adiciona o botão de exportação
-            export_button = st.button("Preparar dados para exportação")
-    
-            # Se o botão for pressionado, exporta o DataFrame
-            if export_button:
-                export_data = BytesIO()
-                df.to_csv(export_data, index=False)
-                export_data.seek(0)
-                st.download_button(
-                    label="Download",
-                    data=export_data,
-                    file_name="dados_filtrados.csv",
-                    key="download_button",
-                )
-            
-            return df
-    
-    # Mapeamento de colunas para nomes mais usuais
+
+    col1, col2, col3 = st.columns([1, 1, 2])
+
+    # Carregar os dados do CSV
+    url = "https://raw.githubusercontent.com/AndersonEduardo/pbl-2023/main/sample_data_clean.csv"
+    df = pd.read_csv(url)
+
+    # Converter colunas para tipo de data
+    df['dh_admissao_paciente'] = pd.to_datetime(df['dh_admissao_paciente'])
+    df['dh_alta_paciente'] = pd.to_datetime(df['dh_alta_paciente'])
+
+    # Mapeamento das colunas
     column_mapping = {
         'dh_admissao_paciente': 'Data de Admissão do Paciente',
         'dh_alta_paciente': 'Data de Alta do Paciente',
@@ -147,17 +82,77 @@ if check_password():
         'ds_resultado_exame': 'Resultado do Exame',
     }
 
-    # Carrega os dados do arquivo CSV no sistema
-    df = pd.read_csv("https://raw.githubusercontent.com/AndersonEduardo/pbl-2023/main/sample_data_clean.csv")
+    # Colunas específicas para filtragem
+    filter_columns = [
+        'ds_tipo_encontro',
+        'ds_unidade_coleta',
+        'ds_predio_coleta',
+        'ds_ala_coleta',
+        'ds_quarto_coleta',
+        'ds_leito_coleta',
+        'cd_sigla_microorganismo',
+        'ds_micro_organismo',
+        'ds_antibiotico_microorganismo',
+        'cd_interpretacao_antibiograma',
+        'ic_crescimento_microorganismo',
+    ]
 
-    # Filtra apenas as colunas mencionadas no column_mapping
-    filtered_df = df[list(column_mapping.keys())]
+    # Layout da aplicação usando múltiplas colunas
+    col1, col2, col3 = st.columns([1, 1, 2])
 
-    # Renomeia as colunas conforme o mapeamento
-    filtered_df = filtered_df.rename(columns=column_mapping)
+    # Botões para seleção de período
+    periodo_opcao = col1.radio("Selecione o período:", ["30 dias", "90 dias", "6 meses", "1 ano", "Período completo"])
 
-    # Mostra o DataFrame filtrado e adiciona a funcionalidade de exportação
-    filtered_df = filter_dataframe(filtered_df)
-    st.dataframe(filtered_df)
+    # Calcular o período correspondente
+    if periodo_opcao == "30 dias":
+        data_maxima = df['dh_admissao_paciente'].max().date()
+        data_inicial = data_maxima - pd.DateOffset(days=30)
+    elif periodo_opcao == "90 dias":
+        data_maxima = df['dh_admissao_paciente'].max().date()
+        data_inicial = data_maxima - pd.DateOffset(days=90)
+    elif periodo_opcao == "6 meses":
+        data_maxima = df['dh_admissao_paciente'].max().date()
+        data_inicial = data_maxima - pd.DateOffset(months=6)
+    elif periodo_opcao == "1 ano":
+        data_maxima = df['dh_admissao_paciente'].max().date()
+        data_inicial = data_maxima - pd.DateOffset(years=1)
+    else:  # Período completo
+        data_inicial = df['dh_admissao_paciente'].min().date()
+        data_maxima = df['dh_admissao_paciente'].max().date()
+
+    # Adicionar filtro de período
+    filtro_periodo = col1.date_input('Período Personalizado:', [data_inicial, data_maxima])
+
+    # Aplicar filtros ao DataFrame
+    df_filtrado = df[(df['dh_admissao_paciente'].dt.date >= filtro_periodo[0]) & (df['dh_admissao_paciente'].dt.date <= filtro_periodo[1])]
+
+    # Renomear elementos do filtro principal de acordo com o mapeamento
+    renamed_mapping = {v: k for k, v in column_mapping.items()}
+    renamed_filters = [renamed_mapping.get(col, col) for col in df_filtrado.columns]
+    df_filtrado.columns = renamed_filters
+
+    # Adicionar checkbox para filtragem de colunas no subfiltro
+    filter_columns_sub = col2.multiselect("Escolha as colunas para filtragem", filter_columns, format_func=lambda x: column_mapping[x])
+    
+    # Adicionar multiselect para selecionar dados dentro de cada coluna escolhida no subfiltro
+    selected_data = {}
+    for col in filter_columns_sub:
+        unique_values = df_filtrado[col].unique()
+        selected_data[col] = col2.multiselect(f"Selecione {column_mapping[col]}", unique_values)
+
+    # Aplicar filtros adicionais
+    for col, values in selected_data.items():
+        if values:
+            df_filtrado = df_filtrado[df_filtrado[col].isin(values)]
+
+    # Obter todos os dados que correspondem aos filtros selecionados
+    df_final = df_filtrado[column_mapping.keys()]
+
+    # Renomear colunas do DataFrame final de acordo com o column_mapping
+    df_final.columns = [column_mapping[col] for col in df_final.columns]
+
+    # Exibir DataFrame resultante
+    col3.dataframe(df_final)
+    
 else:
     st.stop()
